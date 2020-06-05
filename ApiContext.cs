d4054478778bdf41af4072269584a6391d95464a
AthulDilip <athul@fantacode.com>
2020-06-05 22:00:09 +0530

    First Message

---
+++ ApiContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApp.Models;
+
+namespace TodoApp
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ Controllers/TodoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    [ApiController]
+    [Route("todo")]
+    public class TodoController : ControllerBase
+    {
+        private ApiContext _context;
+        public TodoController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> Get()
+        {
+            return _context.TodoItems.Select(x => x);
+        }
+
+        [HttpPost]
+        public IActionResult Add(TodoItem todoItem)
+        {
+            if (String.IsNullOrEmpty(todoItem.Name))
+                return BadRequest();
+
+            var added = _context.TodoItems.Add(todoItem);
+            _context.SaveChanges();
+            return Ok(todoItem);
+        }
+    }
+}
